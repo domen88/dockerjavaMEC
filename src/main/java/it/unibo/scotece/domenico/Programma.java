64c99fabc6f57f238ace11d159fa57720a3adb9d
Domenico Scotece <domen.88@live.it>
2017-07-26 18:40:38 +0200

    handoff
    handoff procedure

--- src/main/java/it/unibo/scotece/domenico/Programma.java
+++ src/main/java/it/unibo/scotece/domenico/Programma.java
@@ -1,12 +1,39 @@
 package it.unibo.scotece.domenico;
 
+import com.spotify.docker.client.DockerClient;
+import it.unibo.scotece.domenico.services.impl.DockerConnectImpl;
+import it.unibo.scotece.domenico.services.impl.ServerSocketSupportImpl;
+
 import static spark.Spark.*;
 
 public class Programma {
 
     public static void main(String[] args){
 
+        //Current docker connector
+        DockerConnectImpl currentDockerConnector =  new DockerConnectImpl();
+        final StringBuffer address = new StringBuffer();
+
         //Set Java Spark server port
-        port(8008);
+        port(8080);
+
+        before((request, response) -> response.type("application/json"));
+
+        get("/connect", "application/json", (req, res) -> {
+
+            DockerClient docker = currentDockerConnector.setConnection();
+            address.append("localhost");
+            return "{\"message\":\"Connect OK\"}";
+
+        });
+
+        get("/socket", "application/json", (req, res) -> {
+
+            ServerSocketSupportImpl socketSupport = new ServerSocketSupportImpl();
+            socketSupport.startServer();
+
+            return "{\"message\":\"Socket Opened\"}";
+
+        });
     }
 }
---
+++ src/main/java/it/unibo/scotece/domenico/services/impl/ServerSocketSupportImpl.java
@@ -0,0 +1,67 @@
+package it.unibo.scotece.domenico.services.impl;
+
+import it.unibo.scotece.domenico.services.SocketSupport;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ServerSocketSupportImpl implements SocketSupport {
+
+    public static final int BUFFER_SIZE = 1024 * 50;
+    private byte[] buffer;
+
+    public ServerSocketSupportImpl(){
+        this.buffer = new byte[BUFFER_SIZE];
+    }
+
+    @Override
+    public void startServer() throws IOException {
+        ServerSocket socket = new ServerSocket(9000);
+        Socket client = socket.accept();
+        final String backup = System.getProperty("user.home") + "/backup.tar";
+
+        BufferedInputStream in =
+                new BufferedInputStream(client.getInputStream());
+
+        BufferedOutputStream out =
+                new BufferedOutputStream(new FileOutputStream(backup));
+
+        int len = 0;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+            System.out.print("#");
+        }
+
+        in.close();
+        out.flush();
+        out.close();
+        client.close();
+        socket.close();
+        System.out.println("\nDone!");
+    }
+
+    @Override
+    public void startClient(String address) throws Exception {
+        Socket socket = new Socket(address, 9000);
+        final String backup = System.getProperty("user.home") + "/backup.tar";
+
+        BufferedInputStream in =
+                new BufferedInputStream(
+                        new FileInputStream(backup));
+
+        BufferedOutputStream out =
+                new BufferedOutputStream(socket.getOutputStream());
+
+        int len = 0;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+            System.out.print("#");
+        }
+        in.close();
+        out.flush();
+        out.close();
+        socket.close();
+        System.out.println("\nDone!");
+    }
+}
