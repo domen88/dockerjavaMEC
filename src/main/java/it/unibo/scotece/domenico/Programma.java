d42236954f40dba11be0e08ad9402f1adc42826a
domen88 <domen.88@live.it>
2017-07-26 09:44:15 +0200

    first commit
    first commit for docker java MEC project

---
+++ src/main/java/it/unibo/scotece/domenico/Programma.java
@@ -0,0 +1,12 @@
+package it.unibo.scotece.domenico;
+
+import static spark.Spark.*;
+
+public class Programma {
+
+    public static void main(String[] args){
+
+        //Set Java Spark server port
+        port(8008);
+    }
+}
